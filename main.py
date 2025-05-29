658092f3d743740fdae88579d0c9e734afeffea2
pallav saikia <pallav.saikia@modusetp.com>
2025-05-29 22:56:03 +0530

    added readme.md

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from server.api.v1.router import router as v1_router
 load_dotenv()
 
 app = FastAPI()
-app.include_router(v1_router)
+
 
 UPLOAD_DIR = os.path.join(os.getcwd(), "uploads")
 os.makedirs(UPLOAD_DIR, exist_ok=True) 
@@ -39,31 +39,4 @@ register_tortoise(
 )
 
 
-
-
-
-# @app.post("/users/", response_model=UserResponse)
-# async def create_user(user: UserCreate):
-#     user_obj = await User.create(**user.model_dump())
-#     return UserResponse.model_validate(user_obj)
-
-# @app.get("/users/{user_id}", response_model=UserResponse)
-# async def get_user(user_id: int):
-#     user = await User.get_or_none(id=user_id)
-#     if not user:
-#         raise HTTPException(status_code=404, detail="User not found")
-#     return UserResponse.model_validate(user)
-
-# @app.get("/users/", response_model=list[UserResponse])
-# async def get_users(skip: int = 0, limit: int = 100):
-#     users = await User.all().offset(skip).limit(limit)
-#     return [UserResponse.model_validate(user) for user in users]
-
-# @app.put("/users/{user_id}", response_model=UserResponse)
-# async def update_user(user_id: int, user_data: UserCreate):
-#     user = await User.get_or_none(id=user_id)
-#     if not user:
-#         raise HTTPException(status_code=404, detail="User not found")
-    
-#     await user.update_from_dict(user_data.model_dump()).save()
-#     return UserResponse.model_validate(user)
+app.include_router(v1_router)
